fa5179ab2894cdd17970df1b5fa7a9e02b3e2100
Louis-Marie Beyer <louismarie.beyer@gmail.com>
2025-06-01 20:19:16 +0200

    gitpages test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: process.env.NODE_ENV === 'production' ? '/Three-JS--Computer/' : '/'
 };
 
 export default config;
