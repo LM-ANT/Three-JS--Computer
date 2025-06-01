3575a44fd243a51057ef2ce0678931040c79def6
Louis-Marie Beyer <louismarie.beyer@gmail.com>
2025-06-01 20:21:46 +0200

    Fix GitHub Pages deployment configuration

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,10 @@ const config = {
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
-		
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/Three-JS--Computer' : ''
+		}
 	}
 };
-
-
 
 export default config;
